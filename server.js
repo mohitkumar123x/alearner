f8e737744bd7ac6d193c72fef5eec393ecbb5d51
MOHIT KUMAR <mohitkumar8909647323@gmail.com>
2018-07-30 00:33:58 +0530

    files in git

---
+++ server.js
@@ -0,0 +1,48 @@
+const ex=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=ex();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/Partials');
+hbs.registerHelper('currentyear',()=>{return new Date().getFullYear()});
+ app.use((req,res,next)=>
+                        { var now= new Date().toString();
+                          log=`${now}: ${req.method} ${req.url}`;
+                          fs.appendFile('server.log',log+'\n',(error)=>{
+                                                                        if(error)console.log("unable to append File:server.log",error);
+                                                                       }
+                                       );
+                          next();
+                        }
+        );
+//app.use((req,res,next)=>{res.render('./partials/maintainance.hbs');});
+app.use(ex.static(__dirname+'/public'));
+
+app.get('/bad',(req,res)=>{res.send('<h1> bad root</h1>')});
+app.get('/',(req,res)=>{
+                            res.render(
+                                        'home.hbs',
+                                        {
+                                          pagetitle:'about',
+
+                                        }
+                                      //reasons: '['server failiure','bad request','etc']'
+                                      );
+
+                           }
+        );
+        app.get('/about',(req,res)=>{
+                                    res.render(
+                                                'webpage.hbs',
+                                                {
+                                                  pagetitle:'about',
+
+                                                }
+                                              );
+
+                                   }
+                );
+app.listen(3000,
+           ()=>{console.log("server is running at port:3000");}
+          );
